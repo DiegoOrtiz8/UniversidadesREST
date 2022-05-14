ce98fb41fcef132666b1a30885a7fce900c2599c
DiegoOrtiz8 <diego.carranza7591@alumno.udg.mx>
2022-05-14 15:59:49 -0500

    Repositorio y Servicios de carrera agregados

---
+++ src/main/java/com/ibm/academia/apirest/Comandos.java
@@ -0,0 +1,20 @@
+package com.ibm.academia.apirest;
+
+import com.ibm.academia.apirest.entities.Carrera;
+import com.ibm.academia.apirest.services.CarreraDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Comandos implements CommandLineRunner {
+    @Autowired
+    private CarreraDAO carreraDAO;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Carrera finanzas = new Carrera(null, "Ingenieria en finanzas", 20, 3);
+        Carrera carreraGuardada = carreraDAO.guardar(finanzas);
+        System.out.println(carreraGuardada.toString());
+    }
+}
