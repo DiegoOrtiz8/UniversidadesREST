07d668bc9093a6e4ae533aec583c37927bb65000
DiegoOrtiz8 <diego.carranza7591@alumno.udg.mx>
2022-05-15 23:39:51 -0500

    Repositorio de empleado completo

---
+++ src/main/java/com/ibm/academia/apirest/repositories/EmpleadoRepository.java
@@ -0,0 +1,13 @@
+package com.ibm.academia.apirest.repositories;
+
+import com.ibm.academia.apirest.entities.Persona;
+import com.ibm.academia.apirest.enums.TipoEmpleado;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository("repositorioEmpleados")
+public interface EmpleadoRepository extends PersonaRepository{
+
+    @Query("select e From Empleado e where e.tipoEmpleado like %?1%")
+    public Iterable<Persona> findEmpleadoByTipoEmpleado(TipoEmpleado tipoEmpleado);
+}
---
+++ src/main/java/com/ibm/academia/apirest/services/EmpleadoDAOImpl.java
@@ -0,0 +1,20 @@
+package com.ibm.academia.apirest.services;
+
+import com.ibm.academia.apirest.entities.Persona;
+import com.ibm.academia.apirest.enums.TipoEmpleado;
+import com.ibm.academia.apirest.repositories.PersonaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class EmpleadoDAOImpl extends PersonaDAOImpl implements EmpleadoDAO{
+
+    @Autowired
+    public EmpleadoDAOImpl(@Qualifier("repositorioEmpleados")PersonaRepository repository) {
+        super(repository);
+    }
+
+    @Override
+    public Iterable<Persona> findEmpleadoByTipoEmpleado(TipoEmpleado tipoEmpleado) {
+        return ((EmpleadoDAO)repository).findEmpleadoByTipoEmpleado(tipoEmpleado);
+    }
+}
